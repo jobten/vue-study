2a07a64b565d52ddb1751c2ce70d6f7c76fa8955
Will <1017665401@qq.com>
2019-07-19 17:06:30 +0800

    分多个项目

---
+++ kata02/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <FormExample></FormExample> -->
+    <TreeExample></TreeExample>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import FormExample from './components/form/'
+import TreeExample from './components/tree/'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    // FormExample,
+    TreeExample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
